d68c4c0d00426023b012dc6e741b62d5e5a1ee38
Hateem954 <hateemnisar@gmail.com>
2025-12-06 23:28:27 +0500

    working on splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -41,7 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       // body: Center(
       //   child: Image.asset(
       //     AppImages.logo,
